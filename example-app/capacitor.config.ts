f21964472f480252566e46370d53212ad1df09ef
Martin Donadieu <martindonadieu@gmail.com>
2025-10-10 04:54:32 +0100

    chore: rename app

--- example-app/capacitor.config.ts
+++ example-app/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'ee.forgr.capacitor_appinsights.example_app',
+  appId: 'app.capgo.appinsights',
   appName: 'example-app',
   webDir: 'dist'
 };
