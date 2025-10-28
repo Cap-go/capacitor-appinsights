4cc02a4d2790cf7d6b147b6d3828f7d7427e4181
Martin Donadieu <martindonadieu@gmail.com>
2025-10-28 00:30:12 +0000

    fix: remove unused echo method from CapacitorAppInsightsPlugin

--- ios/Sources/CapacitorAppInsightsPlugin/CapacitorAppInsightsPlugin.swift
+++ ios/Sources/CapacitorAppInsightsPlugin/CapacitorAppInsightsPlugin.swift
@@ -11,16 +11,8 @@ public class CapacitorAppInsightsPlugin: CAPPlugin, CAPBridgedPlugin {
     public let identifier = "CapacitorAppInsightsPlugin"
     public let jsName = "CapacitorAppInsights"
     public let pluginMethods: [CAPPluginMethod] = [
-        CAPPluginMethod(name: "echo", returnType: CAPPluginReturnPromise),
         CAPPluginMethod(name: "getPluginVersion", returnType: CAPPluginReturnPromise)
     ]
-
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.echo(value)
-        ])
-    }
 
     @objc func getPluginVersion(_ call: CAPPluginCall) {
         call.resolve(["version": self.PLUGIN_VERSION])
